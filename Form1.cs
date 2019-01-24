daf349f778f43a591ad07dc5564f8ec8ea089aa5
szefo94 <szemarcin@gmail.com>
2019-01-24 18:28:53 +0100

    cleaning up the code

--- Form1.cs
+++ Form1.cs
@@ -3,46 +3,42 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.OleDb;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Encog;
+
 namespace ScorePredictor
 {
     public partial class ScorePredictor : Form
     {
-        OpenFileDialog openFile = new OpenFileDialog();
-        
-        string[] lines;
+        private readonly OpenFileDialog openFile = new OpenFileDialog();
+
+        private string[] lines;
         private string fileName;
-        DataTable dt = new DataTable();
-        private List<int> resultsA= new List<int>();
-        private List<int> resultsB= new List<int>();
-        public static double[][] XORIdeal = {
+        private readonly DataTable dt = new DataTable();
+        private readonly List<int> resultsA = new List<int>();
+        private readonly List<int> resultsB = new List<int>();
+
+        public static double[][] XORIdeal =
+        {
             new[] {0.0},
             new[] {1.0},
             new[] {1.0},
             new[] {0.0}
         };
+
         public ScorePredictor()
         {
             InitializeComponent();
         }
-        
+
         private void btnLoadFile_Click(object sender, EventArgs e)
         {
-
             //saving data from csv to DataTable
             if (openFile.ShowDialog() == DialogResult.OK)
             {
                 labelStatus.Text = "file opened";
-                
+
                 fileName = openFile.FileName;
                 lines = File.ReadAllLines(fileName);
                 if (lines.Length > 0)
@@ -60,26 +56,11 @@ namespace ScorePredictor
                         dt.Rows.Add(dr);
                     }
                 }
-             
-                if (dt.Rows.Count > 0) dataGridView1.DataSource = dt;
-               
-            }
-            updateComboBoxes();
-//            if (comboA.Items.Contains(dt.Rows[5][6].ToString()))
-//            {
-//                labelHelper.Text = "contains";
-//            }
-//            else
-//            {
-//                labelHelper.Text = "not contains";
-//            }
-//            labelPrediction.Text = dt.Rows[5][6].ToString();
-            //Wyświetla HOME
-            //labelPrediction.Text = dataGridView1.Columns[5].Name;
-            //Wyświetla AWAY
-            //labelPrediction.Text = dataGridView1.Columns[6].Name;
-            
 
+                if (dt.Rows.Count > 0) dataGridView1.DataSource = dt;
+            }
+
+            updateComboBoxes();
         }
 
         private void updateComboBoxes()
@@ -97,28 +78,24 @@ namespace ScorePredictor
                     comboB.Items.Add(dt.Rows[i][j]);
                 }
         }
-        
+
         private void btnPredict_Click(object sender, EventArgs e)
         {
-            var matchCount=0;
+            var matchCount = 0;
             labelStatus.Text = "searching for matches";
-            labelHelper.Text=comboA.SelectedItem.ToString();
-            
-            int n = 0;// n jako licznik tabeli A
-            int scoreA=0, scoreB=0;
+            labelHelper.Text = comboA.SelectedItem.ToString();
+
+            var n = 0; // n jako licznik tabeli A
+            int scoreA = 0, scoreB = 0;
             for (var k = 0; k < dt.Rows.Count; k++)
                 if (comboA.SelectedItem.ToString().Equals(dt.Rows[k][5].ToString()) &&
                     comboB.SelectedItem.ToString().Equals(dt.Rows[k][6].ToString()))
                 {
                     if (int.Parse(dt.Rows[k][7].ToString()) > int.Parse(dt.Rows[k][8].ToString()))
                     {
-                        
                         scoreA += 2;
                         resultsA.Add(2);
                         resultsB.Add(0);
-                        //labelPrediction.Text += resultsA[n].ToString();
-
-
                     }
                     else if (int.Parse(dt.Rows[k][7].ToString()) == int.Parse(dt.Rows[k][8].ToString()))
                     {
@@ -127,8 +104,6 @@ namespace ScorePredictor
 
                         resultsA.Add(1);
                         resultsB.Add(1);
-
-                        //labelPrediction.Text += resultsA[n].ToString();
                     }
                     else
                     {
@@ -136,9 +111,8 @@ namespace ScorePredictor
 
                         resultsA.Add(0);
                         resultsB.Add(2);
-
-                        //labelPrediction.Text += resultsA[n].ToString();
                     }
+
                     n++;
                     matchCount++;
                 }
@@ -150,8 +124,6 @@ namespace ScorePredictor
                         scoreA += 2;
                         resultsA.Add(2);
                         resultsB.Add(0);
-
-                        //labelPrediction.Text += resultsA[n].ToString();
                     }
                     else if (int.Parse(dt.Rows[k][7].ToString()) == int.Parse(dt.Rows[k][8].ToString()))
                     {
@@ -159,15 +131,12 @@ namespace ScorePredictor
                         scoreA++;
                         resultsA.Add(1);
                         resultsB.Add(1);
-
-                        //labelPrediction.Text += resultsA[n].ToString();
                     }
                     else
                     {
                         scoreB += 2;
                         resultsA.Add(0);
                         resultsB.Add(2);
-                        //labelPrediction.Text += resultsA[n].ToString();
                     }
 
                     n++;
@@ -176,37 +145,27 @@ namespace ScorePredictor
 
             labelA.Text = scoreA.ToString();
             labelB.Text = scoreB.ToString();
-            
+
             label3.Text = "Matches count: ";
             labelStatus.Text = matchCount.ToString();
 
-            Int32[,] results = new Int32[resultsA.Count + 1, 2];
-            for (int g = 0; g < resultsA.Count;g++)
-            {
-                results[g,0]=resultsA[g];
-            }
-            for (int g = 0; g < resultsB.Count;g++)
-            {
-                results[g,1]=resultsB[g];
-            }
-            String resultString = (((double)scoreA / ((double)scoreA + (double)scoreB)) * 100).ToString("#") + "%";
+            var results = new int[resultsA.Count + 1, 2];
+            for (var g = 0; g < resultsA.Count; g++) results[g, 0] = resultsA[g];
+            for (var g = 0; g < resultsB.Count; g++) results[g, 1] = resultsB[g];
+
+            var resultString = (scoreA / (scoreA + (double) scoreB) * 100).ToString("#") + "%";
+
             labelPrediction.Text = resultString;
             label3.Text = "Status:";
             labelStatus.Text = "Predicted";
-
-
-
-
         }
 
         private void comboB_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
 
         private void labelStatus_Click(object sender, EventArgs e)
         {
-
         }
     }
 }
